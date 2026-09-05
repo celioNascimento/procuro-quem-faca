f3faeb90276f4bd31917b7b2df5900ff0dbece0b
Celio Roberto Ramos do Nascimento <celio.gih@gmail.com>
2026-09-05 19:05:56 -0300

    Add new service categories to SUGESTOES_FALLBACK

--- config/categorias.ts
+++ config/categorias.ts
@@ -1,9 +1,54 @@
 //config/categorias.ts
 
 export const SUGESTOES_FALLBACK = [
+  // Manutenção (mantidas)
   'Pedreiro',
   'Eletricista',
   'Encanador',
   'Pintor',
   'Técnico de Ar-condicionado',
+
+  // Beleza & bem-estar
+  'Cabeleireiro',
+  'Manicure',
+  'Maquiador',
+  'Massoterapeuta',
+  'Personal Trainer',
+
+  // Educação & aulas
+  'Professor de Inglês',
+  'Professor de Matemática',
+  'Instrutor de Música',
+  'Explicador',
+
+  // Tecnologia
+  'Técnico em Informática',
+  'Desenvolvedor de Sites',
+  'Designer Gráfico',
+
+  // Casa & serviços domésticos
+  'Diarista',
+  'Cozinheiro',
+  'Babá',
+  'Cuidador de Idosos',
+  'Dog Walker',
+
+  // Eventos & entretenimento
+  'Fotógrafo',
+  'DJ',
+  'Buffet',
+  'Decorador de Festas',
+
+  // Saúde
+  'Nutricionista',
+  'Fisioterapeuta',
+  'Psicólogo',
+
+  // Transporte & mudança
+  'Motorista Particular',
+  'Carregador de Mudança',
+
+  // Jurídico & financeiro
+  'Advogado',
+  'Contador',
 ]
